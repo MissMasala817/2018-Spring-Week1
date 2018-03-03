52cc5f3c723fa6d0749cd4d6504aeef77828b6c6
Masala Thomas <mk.thomas817@gmail.com>
2018-03-03 12:25:22 -0600

    Make our first javascript program

---
+++ test.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var name = "Masala";
+var born = 1995;
+var year = 2018;
+var age = year - born;
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+
+if ( age > 40) {
+	console.log("Damn... you are OLD!!!");
+} else {
+	console.log("Dang... I was hoping to be younger than you.")}
+	if (age == 40) {
+		console.log("Soon you will be old as well.")
+	} else {
+		console.log("I hate you.");
+	}
+
+
